2700ae368887c73981f84ed4fedf481b85dfc887
maotingfeng <maotingfeng@gmail.com>
2015-09-22 21:38:50 +0800

    包初尝试

---
+++ hello/index.js
@@ -0,0 +1,10 @@
+/**
+ *
+ * @authors supmain (mtingfeng@gmail.com)
+ * @date    2015-09-22 21:35:02
+ * @version $Id$
+ */
+
+exports.sayHello = function(){
+    console.log("Hello maotingfeng");
+}
