c89974dfe7f3b60567e46af1f41ee0189ab0bf14
maotingfeng <maotingfeng@gmail.com>
2015-09-22 21:38:36 +0800

    包初尝试

--- app.js
+++ app.js
@@ -1,15 +1,7 @@
 
 
-var EventEmitter = require("events").EventEmitter ;
-var event = new EventEmitter();
-
-
-event.on("some_event",function(){
-    console.log( "some_event发生了" );
-});
+var hello = require("./hello") ;
 
 
 
-setTimeout(function(){
-    event.emit("some_event");
-},1000);
+hello.sayHello();
