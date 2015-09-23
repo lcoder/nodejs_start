fdd6bbb56008bfae6d31a1183162feabb7755dd0
maotingfeng <maotingfeng@gmail.com>
2015-09-23 21:54:34 +0800

    文件操作open,read

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 var fs = require("fs");
 
-fs.readFile("index.txt","utf-8",function(err,data){
-    if(err){
+fs.open("index.txt","r",function(err,fd){
+    if( err ){
         console.error( err );
-    }else {
-        console.log("数据:"+data);
+        return ;
     }
+    var buf = new Buffer( 8 );
+
+    fs.read( fd , buf , 0 , 8 , null , function(err,bytesRead,buffer){
+        if( err ){
+            console.error( err );
+            return ;
+        }
+        console.log( "bytesRead:" + bytesRead );
+        console.log( buffer );
+    } );
 });
-
-
 
 
 
