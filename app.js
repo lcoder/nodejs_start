5c79de2527619140f913cd899ae6183d56e2aa68
maotingfeng <maotingfeng@gmail.com>
2015-09-23 21:28:54 +0800

    事件发射器demo

--- app.js
+++ app.js
@@ -3,16 +3,4 @@ var events = require("events");
 
 var emitter = new events.EventEmitter();
 
-var a = null
-emitter.on("someEvent",function( arg1 , arg2 ){
-    a = arguments.callee ;
-    console.log(arg1,arg2);
-});
-
-
-
-emitter.emit("someEvent","byvoid",1991);
-
-
-
-a(1,2)
+emitter.emit("error");
