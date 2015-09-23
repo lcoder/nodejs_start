007d04d7d7c914f407eaec34ad84e5b5759007d5
maotingfeng <maotingfeng@gmail.com>
2015-09-23 21:45:44 +0800

    文件操作readFile

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
-var events = require("events");
+var fs = require("fs");
+
+fs.readFile("index.txt","utf-8",function(err,data){
+    if(err){
+        console.error( err );
+    }else {
+        console.log("数据:"+data);
+    }
+});
 
 
-var emitter = new events.EventEmitter();
 
-emitter.emit("error");
+
+
