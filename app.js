83a0372e175069a9c82aa0373d79f28bbc72411e
maotingfeng <maotingfeng@gmail.com>
2015-09-24 20:25:22 +0800

    http.serverRequest对象的属性

--- app.js
+++ app.js
@@ -1,21 +1,32 @@
-var fs = require("fs");
 
-fs.open("index.txt","r",function(err,fd){
-    if( err ){
-        console.error( err );
-        return ;
-    }
-    var buf = new Buffer( 8 );
 
-    fs.read( fd , buf , 0 , 8 , null , function(err,bytesRead,buffer){
-        if( err ){
-            console.error( err );
-            return ;
-        }
-        console.log( "bytesRead:" + bytesRead );
-        console.log( buffer );
-    } );
+var http = require("http");
+
+
+var server = new http.Server();
+
+server.on("request",function(req,res){
+
+    console.log(req.complete);
+    console.log( req.httpVersion );
+    console.log( req.method );
+    console.log( req.url );
+    console.log( req.headers );
+    console.log( req.trailers );
+    //console.log( req.connection );
+    //console.log( req.socket );
+    //console.log( req.client );
+    res.writeHead(200,{ "Content-type": "text/html" });
+    res.write("<h1>maotingfeng</h2>");
+    res.end("hello world");
 });
+
+
+server.listen(8080);
+
+
+console.log("开始监听");
+
 
 
 
