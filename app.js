332dedafb35ab5893b0df35c1aba15b3f10e34fd
maotingfeng <maotingfeng@gmail.com>
2015-09-24 20:33:28 +0800

    获取get请求的内容

--- app.js
+++ app.js
@@ -1,24 +1,16 @@
 
 
 var http = require("http");
-
+var url = require("url");
+var util = require("util");
 
 var server = new http.Server();
 
 server.on("request",function(req,res){
 
-    console.log(req.complete);
-    console.log( req.httpVersion );
-    console.log( req.method );
-    console.log( req.url );
-    console.log( req.headers );
-    console.log( req.trailers );
-    //console.log( req.connection );
-    //console.log( req.socket );
-    //console.log( req.client );
     res.writeHead(200,{ "Content-type": "text/html" });
-    res.write("<h1>maotingfeng</h2>");
-    res.end("hello world");
+    res.end( util.inspect( url.parse(req.url , true) ) );
+    console.log( url.parse(req.url , true).query.mao );
 });
 
 
