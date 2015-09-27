b0a3231c93285876f0d210bbd8e4e33f56bcd1ba
maotingfeng <maotingfeng@gmail.com>
2015-09-27 22:55:54 +0800

    微博引用开始

--- microblog/app.js
+++ microblog/app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var partials = require('express-partials');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -14,7 +15,8 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
+app.set('view options',{ layout: true });
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -23,10 +25,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(partials());
 
 app.use('/', routes);
 app.use('/users', users);
-app.use("/user/:username",hello);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- microblog/routes/index.js
+++ microblog/routes/index.js
@@ -6,4 +6,40 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+// routes.user
+router.get('/u/:user',function(req,res,next){
+    res.send("data："+ req.params.user );
+});
+
+// routes.post
+router.post('/post',function(req,res,next){
+
+});
+
+// routes.reg
+router.get('/reg',function(req,res,next){
+
+});
+
+// routes.doReg
+router.post('/reg',function(req,res,next){
+
+});
+
+// routes.login
+router.get('/login',function(req,res,next){
+
+});
+
+// routes.doLogin
+router.post('/doLogin',function(req,res,next){
+
+});
+
+// routes.logout
+router.post('/logout',function(req,res,next){
+
+});
+
+
 module.exports = router;
