1cecaa4cb2ddb12b3bb0c4265dec1fb8fcc7f6bf
maotingfeng <maotingfeng@gmail.com>
2016-03-12 10:26:09 +0800

    初始化

---
+++ chatrooms/server.js
@@ -0,0 +1,9 @@
+/**
+ * Created by maotingfeng on 16/3/12.
+ */
+var http = require("http") ;
+var fs = require("fs") ;
+var path = require("path") ;            // 文件系统路径相关
+var mime = require("mime") ;
+var cache = {} ;
+
