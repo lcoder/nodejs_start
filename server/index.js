f6d6538c654c6a9a23183b2fb346ecdfefe3f237
maotingfeng <maotingfeng@gmail.com>
2016-04-17 11:59:48 +0800

    管道pipe

--- server/index.js
+++ server/index.js
@@ -8,13 +8,11 @@ var server = http.createServer( function( req , res ){
     var url = parse( req.url ) ;
     var path = join( root , url.pathname ) ;
     var stream = fs.createReadStream( path ) ;
-    stream.on( 'data' , function( chunk ){
-        res.write( chunk ) ;
+    stream.pipe( res ) ;
+    stream.on( 'error' , function( err ){
+        res.statusCode = 500 ;
+        res.end( '服务器错误' ) ;
     } ) ;
-    stream.on( 'end' , function(){
-        res.end() ;
-    } ) ;
-
 } ) ;
 
 server.listen( 3000 ) ;
