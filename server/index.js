8a6df29e1ddcd4a29dfbaf5b1d018a79e50c0ed5
maotingfeng <maotingfeng@gmail.com>
2016-04-17 12:39:48 +0800

    fs.stat()

--- server/index.js
+++ server/index.js
@@ -7,11 +7,26 @@ var root = __dirname ;
 var server = http.createServer( function( req , res ){
     var url = parse( req.url ) ;
     var path = join( root , url.pathname ) ;
-    var stream = fs.createReadStream( path ) ;
-    stream.pipe( res ) ;
-    stream.on( 'error' , function( err ){
-        res.statusCode = 500 ;
-        res.end( '服务器错误' ) ;
+    console.log( path );
+    fs.stat( path , function( err , stat ){
+        console.log( arguments );
+        if( err ){
+            if( err.code == 'ENOENT' ){
+                res.statusCode = 404 ;
+                res.end( '404没有找到' ) ;
+            }else{
+                res.statusCode = 500 ;
+                res.end( '500内部错误' ) ;
+            }
+        }else{
+            res.setHeader( 'Content-Length' , stat.size ) ;
+            var stream = fs.createReadStream( path ) ;
+            stream.pipe( res ) ;
+            stream.on( 'error' , function( err ){
+                res.statusCode = 500 ;
+                res.end( '500内部错误:stream读取错误' ) ;
+            } ) ;
+        }
     } ) ;
 } ) ;
 
