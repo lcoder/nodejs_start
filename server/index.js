75143959bbbb4e019da08740e86df550be973ac3
毛庭峰 <maotingfeng@maotingfengdeMacBook-Pro.local>
2016-05-08 18:32:35 +0800

    获取application/x-www-form-urlencoded 表单数据

--- server/index.js
+++ server/index.js
@@ -1,33 +1,69 @@
-var http = require('http') ,
-    parse = require('url').parse ,
-    join = require('path').join ,
-    fs = require('fs') ;
-var root = __dirname ;
+var http = require('http') ;
+var qs = require( 'querystring' ) ;
+var items = [] ;
 
 var server = http.createServer( function( req , res ){
-    var url = parse( req.url ) ;
-    var path = join( root , url.pathname ) ;
-    console.log( path );
-    fs.stat( path , function( err , stat ){
-        console.log( arguments );
-        if( err ){
-            if( err.code == 'ENOENT' ){
-                res.statusCode = 404 ;
-                res.end( '404没有找到' ) ;
-            }else{
-                res.statusCode = 500 ;
-                res.end( '500内部错误' ) ;
-            }
-        }else{
-            res.setHeader( 'Content-Length' , stat.size ) ;
-            var stream = fs.createReadStream( path ) ;
-            stream.pipe( res ) ;
-            stream.on( 'error' , function( err ){
-                res.statusCode = 500 ;
-                res.end( '500内部错误:stream读取错误' ) ;
-            } ) ;
+    if( '/' == req.url ){
+        switch( req.method ) {
+            case 'GET':
+                show( res );
+                break;
+            case 'POST':
+                add( req , res ) ;
+                break;
+            default:
+                badRequest( res ) ;
         }
-    } ) ;
+    }else{
+        notFound( res );
+    }
 } ) ;
+
+// 列表
+function show( res ){
+    var html = '<html><head><title>代办事项清单</title></head><body>'
+        + '<h1>列表</h1>'
+        + '<ul>'
+        + items.map( function( item ){
+            return '<li>' + item + '</li>' ;
+        } ).join( '' )
+        + '<form action="/" method="post">'
+        + '<p><input type="text" name="item" /></p>'
+        + '<p><input type="submit" value="添加" /></p>'
+        + '</form>'
+        + '</body>'
+        + '</html>';
+    res.setHeader( 'Content-Type' , 'text/html' ) ;
+    res.setHeader( 'Content-Length' , Buffer.byteLength( html ) ) ;
+    res.end( html ) ;
+}
+
+function notFound( res ){
+    res.statusCode = 404 ;
+    res.setHeader( 'Content-Type' , 'text/plain' ) ;
+    res.end( '404' );
+}
+
+function badRequest( res ){
+    res.statusCode = 400 ;
+    res.setHeader( 'Content-Type' , 'text/plain' ) ;
+    res.end( '恶意请求' );
+}
+
+
+function add( req , res ){
+    var body = '' ;
+    req.setEncoding( 'utf8' ) ;
+    req.on( 'data' , function( chunk ){
+        body += chunk ;
+    } ) ;
+    req.on( 'end' , function(){
+        var obj = qs.parse( body ) ;
+        items.push( obj.item ) ;
+        show( res ) ;
+    } ) ;
+}
+
+
 
 server.listen( 3000 ) ;
