0c6936076e9eac41b23124e4ba938bc06cdd8996
毛庭峰 <maotingfeng@maotingfengdeMacBook-Pro.local>
2016-05-08 21:10:33 +0800

    formidable文件上传模块

--- server/index.js
+++ server/index.js
@@ -1,69 +1,67 @@
 var http = require('http') ;
-var qs = require( 'querystring' ) ;
-var items = [] ;
+var formidable = require('formidable') ;
 
 var server = http.createServer( function( req , res ){
-    if( '/' == req.url ){
-        switch( req.method ) {
-            case 'GET':
-                show( res );
-                break;
-            case 'POST':
-                add( req , res ) ;
-                break;
-            default:
-                badRequest( res ) ;
-        }
-    }else{
-        notFound( res );
+    switch( req.method ) {
+        case 'GET':
+            show( req , res ) ;
+            break;
+        case 'POST':
+            upload( req , res ) ;
+            break;
+        default:
+            console.log( '错误请求' ) ;
     }
 } ) ;
 
-// 列表
-function show( res ){
-    var html = '<html><head><title>代办事项清单</title></head><body>'
-        + '<h1>列表</h1>'
-        + '<ul>'
-        + items.map( function( item ){
-            return '<li>' + item + '</li>' ;
-        } ).join( '' )
-        + '<form action="/" method="post">'
-        + '<p><input type="text" name="item" /></p>'
-        + '<p><input type="submit" value="添加" /></p>'
+// 上传表单
+function show( req , res ){
+    var html = '<html><head><title>文件上传</title></head><body>'
+        + '<h1>上传区域</h1>'
+        + '<form action="/" method="post" enctype="multipart/form-data">'
+        + '<p><input type="text" name="name" /></p>'
+        + '<p><input type="file" name="file" /></p>'
+        + '<p><input type="submit" value="上传" /></p>'
         + '</form>'
         + '</body>'
-        + '</html>';
+        + '</html>' ;
     res.setHeader( 'Content-Type' , 'text/html' ) ;
     res.setHeader( 'Content-Length' , Buffer.byteLength( html ) ) ;
     res.end( html ) ;
 }
 
-function notFound( res ){
-    res.statusCode = 404 ;
-    res.setHeader( 'Content-Type' , 'text/plain' ) ;
-    res.end( '404' );
+function isFormData( req ){
+    var type = req.headers[ 'content-type' ] || '' ;
+    return 0 == type.indexOf( 'multipart/form-data' ) ;
 }
 
-function badRequest( res ){
-    res.statusCode = 400 ;
-    res.setHeader( 'Content-Type' , 'text/plain' ) ;
-    res.end( '恶意请求' );
-}
-
-
-function add( req , res ){
-    var body = '' ;
-    req.setEncoding( 'utf8' ) ;
-    req.on( 'data' , function( chunk ){
-        body += chunk ;
+function upload( req , res ){
+    if( !isFormData( req ) ){
+        res.statusCode = 400 ;
+        res.end('错误请求，必须为multipart/form-data请求类型') ;
+    }
+    var form = new formidable.IncomingForm() ;
+    /*form.on( 'field' , function( field , value ){
+        console.log( field );
+        console.log( value );
     } ) ;
-    req.on( 'end' , function(){
-        var obj = qs.parse( body ) ;
-        items.push( obj.item ) ;
-        show( res ) ;
+    form.on( 'file' , function( name , file ){
+        console.log( name );
+        console.log( file );
+    } ) ;
+    form.on( 'end' , function( ){
+        res.end( '上传完成' ) ;
+    } ) ;*/
+    form.on( 'progress' , function( bytesReceived , bytesExpected ){
+        var percent = Math.floor( bytesReceived / bytesExpected * 100 ) ;
+        console.log( percent ) ;
+    } ) ;
+    form.parse( req , function( err , fields , files ){
+        console.log( fields ) ;
+        console.log( files ) ;
+        res.end( 'upload complete' ) ;
     } ) ;
 }
-
 
 
 server.listen( 3000 ) ;
