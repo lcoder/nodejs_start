5cceed32074d134850d1c37a698ef0071d7f328f
毛庭峰 <maotingfeng@maotingfengdeMacBook-Pro.local>
2016-05-08 21:31:38 +0800

    基于文件系统的存储

--- server/index.js
+++ server/index.js
@@ -1,67 +1,55 @@
-var http = require('http') ;
-var formidable = require('formidable') ;
+var fs = require( 'fs' ) ;
+var path = require( 'path' ) ;
+var args = process.argv.splice( 2 ) ;
+var command = args.shift() ;
+var taskDescription = args.join( ' ' ) ;
+var file = path.join( process.cwd() , '/.tasks' ) ;
 
-var server = http.createServer( function( req , res ){
-    switch( req.method ) {
-        case 'GET':
-            show( req , res ) ;
-            break;
-        case 'POST':
-            upload( req , res ) ;
-            break;
-        default:
-            console.log( '错误请求' ) ;
-    }
-} ) ;
-
-// 上传表单
-function show( req , res ){
-    var html = '<html><head><title>文件上传</title></head><body>'
-        + '<h1>上传区域</h1>'
-        + '<form action="/" method="post" enctype="multipart/form-data">'
-        + '<p><input type="text" name="name" /></p>'
-        + '<p><input type="file" name="file" /></p>'
-        + '<p><input type="submit" value="上传" /></p>'
-        + '</form>'
-        + '</body>'
-        + '</html>' ;
-    res.setHeader( 'Content-Type' , 'text/html' ) ;
-    res.setHeader( 'Content-Length' , Buffer.byteLength( html ) ) ;
-    res.end( html ) ;
+switch( command ) {
+    case 'list':
+        listTasks( file ) ;
+        break;
+    case 'add':
+        addTask( file , taskDescription ) ;
+        break;
+    default:
+        console.log( '控制台的正确使用方法' + process.argv[0] );
+        break;
 }
 
-function isFormData( req ){
-    var type = req.headers[ 'content-type' ] || '' ;
-    return 0 == type.indexOf( 'multipart/form-data' ) ;
+function loadOrInitializeTaskArray( file , cb ){
+    fs.exists( file , function( exists ){
+        var tasks = [] ;
+        if( exists ){
+            fs.readFile( file , 'utf8' , function( err , data ){
+                if( err ) throw err ;
+                var data = data.toString() ;
+                var tasks = JSON.parse( data || '[]' ) ;
+                cb( tasks ) ;
+            } ) ;
+        }else{
+            cb( [] ) ;
+        }
+    } ) ;
 }
-
-function upload( req , res ){
-    if( !isFormData( req ) ){
-        res.statusCode = 400 ;
-        res.end('错误请求，必须为multipart/form-data请求类型') ;
-    }
-    var form = new formidable.IncomingForm() ;
-    /*form.on( 'field' , function( field , value ){
-        console.log( field );
-        console.log( value );
-    } ) ;
-    form.on( 'file' , function( name , file ){
-        console.log( name );
-        console.log( file );
-    } ) ;
-    form.on( 'end' , function( ){
-        res.end( '上传完成' ) ;
-    } ) ;*/
-    form.on( 'progress' , function( bytesReceived , bytesExpected ){
-        var percent = Math.floor( bytesReceived / bytesExpected * 100 ) ;
-        console.log( percent ) ;
-    } ) ;
-    form.parse( req , function( err , fields , files ){
-        console.log( fields ) ;
-        console.log( files ) ;
-        res.end( 'upload complete' ) ;
+function storeTasks( file , tasks ){
+    fs.writeFile( file , JSON.stringify( tasks ) , function( err ){
+        if( err ) throw err ;
+        console.log('保存成功！');
     } ) ;
 }
 
+function listTasks( file ){
+    loadOrInitializeTaskArray( file , function( tasks ){
+        for( var i in tasks ){
+            console.log( tasks[ i ] ) ;
+        }
+    } ) ;
+}
 
-server.listen( 3000 ) ;
+function addTask( file , taskDescription ){
+    loadOrInitializeTaskArray( file , function( tasks ){
+        tasks.push( taskDescription ) ;
+        storeTasks( file , tasks ) ;
+    } )
+}
